b3e02398c9c5d06944dfed0a4c4f204b62dc8136
ibrahim <ibrahim-akin@msn.com>
2017-12-07 17:12:53 +0300

    Add files via upload
    Sayıların yazılışında kaç karakter kullanıldı

---
+++ SayıYazma.py
@@ -0,0 +1,22 @@
+teens = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+def donustur(n):
+    if(n<20):
+        return(teens[n])
+    elif(n<100):
+        return(tens[int(n/10)] + teens[n%10])
+    elif(n<1000):
+        if(n%100==0):
+            return(teens[int(n/100)]+"hundred")
+        else:
+            return(teens[int(n/100)]+"hundred"+"and"+donustur(n%100))
+    else:
+        return("onethousand")
+
+sonuc = 0
+
+for i in range(1,1001):
+    sonuc += len(donustur(i))
+
+print(sonuc)
